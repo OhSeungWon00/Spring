d3043fd0e10990d79495a2a39a6a4674d60370ec
OhSeungWon00 <ajtwodtmdnjs@gmail.com>
2024-11-22 16:20:24 +0900

    security-method 추가

---
+++ security-method/src/main/java/com/aloha/security_method/service/UserService.java
@@ -0,0 +1,25 @@
+package com.aloha.security_method.service;
+
+import com.aloha.security_method.domain.UserAuth;
+import com.aloha.security_method.domain.Users;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public interface UserService {
+
+    // 🔐 로그인 
+    public boolean login(Users user, HttpServletRequest request) throws Exception;
+    
+    // 조회
+    public Users select(String username) throws Exception;
+
+    // 회원 가입
+    public int join(Users user) throws Exception;
+
+    // 회원 수정
+    public int update(Users user) throws Exception;
+
+    // 회원 권한 등록
+    public int insertAuth(UserAuth userAuth) throws Exception;
+
+}
